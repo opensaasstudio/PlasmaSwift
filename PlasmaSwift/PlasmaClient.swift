7d27299f47551870239092b66bd8ada34da9e717
Ryo Aoyama <r.fe51028.r@gmail.com>
2018-06-27 00:04:07 +0900

    Refactor PlasmaClient

--- PlasmaSwift/PlasmaClient.swift
+++ PlasmaSwift/PlasmaClient.swift
@@ -2,9 +2,9 @@ import SwiftGRPC
 import SwiftProtobuf
 
 public final class PlasmaClient {
-    public enum Result {
-        case success(payload: PlasmaPayload)
-        case failure(error: Error)
+    public enum Event {
+        case next(payload: PlasmaPayload)
+        case error(Error)
     }
     
     public static var isDebugLogEnabled = false
@@ -20,7 +20,7 @@ public final class PlasmaClient {
         service.timeout = .greatestFiniteMagnitude
     }
     
-    public func connect(_ eventHandler: @escaping (Result) -> Void) -> Connection {
+    public func connect(_ eventHandler: @escaping (Event) -> Void) -> Connection {
         return .init(service: service, eventHandler: eventHandler)
     }
 }
@@ -28,15 +28,16 @@ public final class PlasmaClient {
 public extension PlasmaClient {
     public final class Connection {
         private let service: PlasmaStreamServiceServiceClient
-        private let eventHandler: (Result) -> Void
+        private let eventHandler: (Event) -> Void
 
         private let reconnectQueue = DispatchQueue(label: "io.github.openfresh.plasma.reconnectQueue")
-        private let call = Atomic<Call?>(nil) { oldValue in
-            oldValue?.cancel()
-        }
+        private let callLock = NSLock()
         private var events: [PlasmaEventType] = []
+        private var call: Call? = nil {
+            didSet { oldValue?.cancel() }
+        }
         
-        fileprivate init(service: PlasmaStreamServiceServiceClient, eventHandler: @escaping (Result) -> Void) {
+        fileprivate init(service: PlasmaStreamServiceServiceClient, eventHandler: @escaping (Event) -> Void) {
             self.service = service
             self.eventHandler = eventHandler
 
@@ -45,40 +46,45 @@ public extension PlasmaClient {
         
         @discardableResult
         public func subscribe(types: [String]) -> Self {
-            let events = types.map(PlasmaEventType.init)
+            callLock.lock()
+            defer { callLock.unlock() }
 
-            call.withValue { call in
-                call?.subscribe(events: events)
-                self.events = events
-            }
+            let events = types.map(PlasmaEventType.init)
+            call?.subscribe(events: events)
+            self.events = events
 
             PlasmaClient.log("sent subscribed events \(types) to plasma")
             return self
         }
         
         public func shutdown() {
-            call.withValue { $0?.cancel() }
+            callLock.lock()
+            defer { callLock.unlock() }
+
+            call?.cancel()
+
             PlasmaClient.log("closed connection")
         }
         
         private func connect(retry: Int) {
-            call.modify { call in
-                call = Call(service: service, events: events) { [weak self] result in
-                    switch result {
-                    case .success(let payload):
-                        PlasmaClient.log("received payload = \(payload)")
-                        
-                    case .failure(let error as RPCError) where error.callResult?.statusCode == .unavailable && retry > 0:
-                        PlasmaClient.log("stream service is gone. \(error.localizedDescription)")
-                        self?.reconnect(after: 5, retry: retry - 1)
-                        return
-                        
-                    case .failure(let error):
-                        PlasmaClient.log("error = \(error.localizedDescription)")
-                    }
-                    
-                    self?.eventHandler(result)
+            callLock.lock()
+            defer { callLock.unlock() }
+
+            call = Call(service: service, events: events) { [weak self] event in
+                switch event {
+                case .next(let payload):
+                    PlasmaClient.log("received payload = \(payload)")
+
+                case .error(let error as RPCError) where error.callResult?.statusCode == .unavailable && retry > 0:
+                    PlasmaClient.log("stream service is gone. \(error.localizedDescription)")
+                    self?.reconnect(after: 5, retry: retry - 1)
+                    return
+
+                case .error(let error):
+                    PlasmaClient.log("error = \(error.localizedDescription)")
                 }
+
+                self?.eventHandler(event)
             }
         }
 
@@ -96,13 +102,18 @@ public extension PlasmaClient {
 private extension PlasmaClient.Connection {
     final class Call {
         private let protoCall: PlasmaStreamServiceEventsCall
-        private let eventHandler: (PlasmaClient.Result) -> Void
+        private let eventHandler: (PlasmaClient.Event) -> Void
 
-        init(service: PlasmaStreamServiceServiceClient, events: [PlasmaEventType], eventHandler: @escaping (PlasmaClient.Result) -> Void) {
-            self.protoCall = try! service.events(completion: nil)
-            self.eventHandler = eventHandler
+        init?(service: PlasmaStreamServiceServiceClient, events: [PlasmaEventType], eventHandler: @escaping (PlasmaClient.Event) -> Void) {
+            do {
+                self.protoCall = try service.events(completion: nil)
+                self.eventHandler = eventHandler
+                subscribe(events: events)
 
-            subscribe(events: events)
+            } catch {
+                eventHandler(.error(error))
+                return nil
+            }
         }
 
         func subscribeReceiveMessage() {
@@ -110,20 +121,20 @@ private extension PlasmaClient.Connection {
                 try protoCall.receive { [weak self] result in
                     switch result {
                     case .result(let payload?):
-                        self?.eventHandler(.success(payload: payload))
+                        self?.eventHandler(.next(payload: payload))
 
                     case .result(.none):
                         return
 
                     case .error(let error):
-                        self?.eventHandler(.failure(error: error))
+                        self?.eventHandler(.error(error))
                     }
 
                     self?.subscribeReceiveMessage()
                 }
 
-            } catch let error {
-                eventHandler(.failure(error: error))
+            } catch {
+                eventHandler(.error(error))
             }
         }
 
@@ -134,8 +145,8 @@ private extension PlasmaClient.Connection {
                 let request = PlasmaRequest(events: events)
                 try protoCall.send(request)
 
-            } catch let error {
-                eventHandler(.failure(error: error))
+            } catch {
+                eventHandler(.error(error))
             }
 
             subscribeReceiveMessage()
@@ -147,8 +158,8 @@ private extension PlasmaClient.Connection {
                 try protoCall.send(request)
                 protoCall.cancel()
 
-            } catch let error {
-                eventHandler(.failure(error: error))
+            } catch {
+                eventHandler(.error(error))
             }
         }
     }
@@ -156,18 +167,20 @@ private extension PlasmaClient.Connection {
 
 private extension PlasmaClient {
     static let dateFormatter: DateFormatter = {
-        var formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .long
         return formatter
     }()
     
     static func log<T>(_ data: @autoclosure () -> T) {
         guard isDebugLogEnabled else { return }
+
+        let now = Date()
+        let dateString = dateFormatter.string(from: now)
+        let log = "\(dateString) [Plasma] \(data())"
         
-        let dateString = dateFormatter.string(from: Date())
-        let fullLog = "\(dateString) [Plasma] \(data())"
-        
-        print(fullLog)
+        print(log)
     }
 }
 
@@ -187,53 +200,5 @@ private extension PlasmaEventType {
     init(type: String) {
         self.init()
         self.type = type
-    }
-}
-
-private final class Atomic<Value> {
-    private var _value: Value {
-        didSet {
-            didSet?(oldValue)
-        }
-    }
-    private let didSet: ((Value) -> Void)?
-    private let lock: NSLock = .init()
-    
-    init(_ value: Value, didSet: ((Value) -> Void)? = nil) {
-        _value = value
-        self.didSet = didSet
-    }
-    
-    var value: Value {
-        get {
-            return withValue { $0 }
-        }
-        set {
-            swap(newValue)
-        }
-    }
-    
-    @discardableResult
-    func modify<Result>(_ action: (inout Value) throws -> Result) rethrows -> Result {
-        lock.lock()
-        defer { lock.unlock() }
-        return try action(&_value)
-    }
-    
-    @discardableResult
-    func withValue<Result>(_ action: (Value) throws -> Result) rethrows -> Result {
-        lock.lock()
-        defer { lock.unlock() }
-        
-        return try action(_value)
-    }
-    
-    @discardableResult
-    func swap(_ newValue: Value) -> Value {
-        return modify { value in
-            let oldValue = value
-            value = newValue
-            return oldValue
-        }
     }
 }
